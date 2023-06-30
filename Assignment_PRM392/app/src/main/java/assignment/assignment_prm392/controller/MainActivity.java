47b6fac1f6566d4184c71ecb4eca098a3e968f78
eternityshine <truongnlnde160015@fpt.edu.vn>
2023-06-30 15:57:08 +0700

    new commit

--- Assignment_PRM392/app/src/main/java/assignment/assignment_prm392/controller/MainActivity.java
+++ Assignment_PRM392/app/src/main/java/assignment/assignment_prm392/controller/MainActivity.java
@@ -5,19 +5,24 @@ import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import assignment.assignment_prm392.R;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ParseObject firstObject = new  ParseObject("FirstClass");
-        firstObject.put("message","Hey ! First message from android. Parse is now connected");
-        firstObject.saveInBackground(e -> {
+        ParseQuery<ParseObject> searchQuery = ParseQuery.getQuery("Employee");
+        searchQuery.findInBackground((objects, e) -> {
             if (e != null){
                 Log.e("MainActivity", e.getLocalizedMessage());
             }else{
-                Log.d("MainActivity","Object saved.");
+                objects.forEach(object -> {
+                    Log.d("Full Name", object.getString("full_name"));
+                    Log.d("Email", object.getString("email"));
+                    Log.d("Gender", object.getBoolean("Gender") ? "Male" : "Female");
+                    Log.d("Email", String.valueOf(object.getNumber("credit_card")));
+                });
             }
         });
     }
